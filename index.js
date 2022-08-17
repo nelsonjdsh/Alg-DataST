a2af408a67eee384e1404ff895008714dc937fa8
nelsonjsh+1-at-410048025254 <1086369@est.intec.edu.do>
2022-08-17 16:25:54 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,41 @@
+function numDiff(x, y) {
+  let diff = 0;
+  if (x && y && x > y) {
+    for (let i = y; i < x; i++) {
+      diff++;
+    }
+  } else if (y && x && y > x) {
+    for (let i = x; i < y; i++) {
+      diff++;
+    }
+  } else {
+    return 0;
+  }
+  return diff;
+}
+
+const maxValue = (arr) => {
+  let max = 0;
+  for (let item of arr) {
+    if (item > max) {
+      max = item;
+    }
+  }
+  return max;
+};
+
+let values = [23, 34, 444];
+// console.log(maxValue(values));
+
+const characterLength = (word) => {
+  let chars = 0;
+  for (let element of word) {
+    if (element.includes(' ')) {
+      return `Value [${word}] is not valid. Word cannot contain spaces`;
+    }
+    chars++;
+  }
+  return chars;
+};
+
+// console.log(characterLength('esqueleto'));
