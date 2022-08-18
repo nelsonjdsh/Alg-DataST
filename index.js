6569aad4693f7ddb124db98e353f505c202bece7
Nelson De Jesus Santos <ndj@Nelsons-MacBook-Air.local>
2022-08-18 13:27:19 -0400

    Push

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function numDiff(x, y) {
   return diff;
 }
 
-const maxValue = (arr) => {
+const maxValue2 = (arr) => {
   let max = 0;
   for (let item of arr) {
     if (item > max) {
@@ -44,3 +44,87 @@ function sumArrayValues(arr) {
   }
   return sum;
 }
+
+const numberDiff = (a, b) => {
+  let diff = 0;
+  while (a > b) {
+    b++;
+    diff++;
+  }
+  return diff;
+};
+
+const numberMulti = (a, b) => {
+  let multi = 0;
+
+  for (let i = 0; i < a; i++) {
+    multi += b;
+  }
+  return multi;
+};
+
+const maxValue = (arr) => {
+  let max = 0;
+
+  for (let element of arr) {
+    if (element > max) {
+      max += element;
+    }
+  }
+  return max;
+};
+
+const numDiv = (a, b) => {
+  let result = 0;
+  while (a >= b) {
+    result++;
+    a -= b;
+  }
+  return result;
+};
+
+console.log(numDiv(8, 6));
+
+function primeNumber(number) {
+  let prime = true;
+  for (let i = 2; i <= number / 2 && prime; i++) {
+    if (number % i == 0) prime = false;
+  }
+  return prime;
+}
+
+console.log(primeNumber(31));
+
+function simpleArraySum(arr) {
+  let sum = 0;
+  for (const element of arr) {
+    sum += element;
+  }
+  return sum;
+}
+
+const compareTriplets = (a, b) => {
+  let index = 0;
+  let alice = 0;
+  let bob = 0;
+  for (const element of a) {
+    if (element < b[index]) {
+      bob++;
+    }
+
+    if (element > b[index]) {
+      alice++;
+    }
+
+    index++;
+  }
+  return [alice, bob];
+};
+
+const arraySum = (ar) => {
+  let sum = 0;
+  for (const element of ar) {
+    sum += element;
+  }
+  return sum;
+};
